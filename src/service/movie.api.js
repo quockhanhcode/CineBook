25e452526de56a8a2f1db84bfe6f5d3af0100805
quockhanhcode <01673khanh@gmail.com>
2025-08-20 23:24:20 +0700

    first commmit

---
+++ src/service/movie.api.js
@@ -0,0 +1,43 @@
+import api from "./api";
+
+export const getListMovieApi = async (maNhom, page, itemPage) => {
+  try {
+    const response = await api.get(
+      `/QuanLyPhim/LayDanhSachPhimPhanTrang?maNhom=${maNhom}&soTrang=${page}&soPhanTuTrenTrang=${itemPage}`
+    );
+    return response.data.content;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const getBannerApi = async () => {
+  try {
+    const response = await api.get("/QuanLyPhim/LayDanhSachBanner");
+    return response.data.content;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const getMovieDetailApi = async (maPhim) => {
+  try {
+    const response = await api.get(
+      `/QuanLyPhim/LayThongTinPhim?MaPhim=${maPhim}`
+    );
+    return response.data.content;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const movie_info = async (maPhim) => {
+  try {
+    const response = await api.get(
+      `/QuanLyRap/LayThongTinLichChieuPhim?MaPhim=${maPhim}`
+    );
+    return response.data.content;
+  } catch (error) {
+    console.log(error);
+  }
+};
