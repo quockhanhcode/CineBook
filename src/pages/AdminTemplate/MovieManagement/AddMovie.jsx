aaa37414712cbc6dc2aeb5404519d2301ddc61c1
quockhanhcode <01673khanh@gmail.com>
2025-09-15 23:40:07 +0700

    fix done get value boolean checked field [hot]

--- src/pages/AdminTemplate/MovieManagement/AddMovie.jsx
+++ src/pages/AdminTemplate/MovieManagement/AddMovie.jsx
@@ -53,9 +53,9 @@ export default function AddMovie() {
   });
 
   const errors = formState.errors;
-  console.log("errors", errors);
 
   const hinhAnh = watch("hinhAnh");
+  const isChecked = watch("hot");
 
   const previewImage = (file) => {
     if (!file) return "";
@@ -88,11 +88,9 @@ export default function AddMovie() {
       ...rest,
       sapChieu: trangThai === "false",
       dangChieu: trangThai === "true",
-      hot: hot === "true",
+      hot: isChecked,
     };
     console.log(newValues);
-
-    console.log("hot", hot);
 
     const formData = new FormData();
     formData.append("tenPhim", newValues.tenPhim);
@@ -105,11 +103,10 @@ export default function AddMovie() {
       "ngayKhoiChieu",
       format(newValues.ngayKhoiChieu, "dd/MM/yyyy")
     );
-
     formData.append("maNhom", newValues.maNhom);
     formData.append("hot", newValues.hot);
     formData.append("hinhAnh", newValues.hinhAnh);
-    // addItemMovies(formData);
+    addItemMovies(formData);
   };
 
   return (
@@ -268,18 +265,13 @@ export default function AddMovie() {
                         )}
                       </div>
                     </div>
-                    <div className="flex flex-col space-y-3.5">
-                      <label className="inline-flex items-center cursor-pointer">
-                        <input
-                          type="checkbox"
-                          {...register("hot")}
-                          className="sr-only peer"
-                        />
-                        <div className="relative w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600 dark:peer-checked:bg-blue-600" />
-                        <span className="ms-3 text-sm font-medium text-gray-900 dark:text-gray-300">
-                          hot
-                        </span>
+                    <div className="flex items-center me-4">
+                      <input type="checkbox" {...register("hot")} />
+                      <label className="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">
+                        Hot
                       </label>
+                    </div>
+                    <div className="flex flex-col space-y-3.5">
                       <label className="inline-flex flex-col cursor-pointer">
                         <span className="text-sm mb-3.5 font-medium text-gray-900 dark:text-gray-300">
                           Tình Trạng
